b003da492eb94d48f71a7a1cbebd58d1443d4eb6
Alan Peixinho <alan.peixinho@pitec.co>
2025-04-29 01:12:08 -0300

    Day 09 cleanup

--- 2024/09/09.rs
+++ 2024/09/09.rs
@@ -1,13 +1,13 @@
-use std::{collections::{HashMap, LinkedList}, io::{stdin, Read}};
+use std::{collections::LinkedList, io::stdin};
 
-fn read_line() -> (Vec<Option<i32>>, LinkedList<(usize, i32)>) {
+fn read_line() -> (Vec<Option<i32>>, Vec<(usize, usize)>) {
     let mut nums: Vec<u32> = Vec::new();
     for line in stdin().lines() {
         nums = line.expect("fail read").chars()
             .filter_map(|c| c.to_digit(10)).collect();
     }
 
-    let mut emptyblocks = LinkedList::new();
+    let mut emptyblocks = Vec::with_capacity(nums.len());
     let mut mem: Vec<Option<i32>> = Vec::with_capacity(nums.len() * 9);
     for (fileid, i) in (0..nums.len()).step_by(2).enumerate() {
         let (nwritten, nempty) = (nums[i], if i+1 < nums.len() { nums[i+1] } else { 0 });
@@ -15,7 +15,7 @@ fn read_line() -> (Vec<Option<i32>>, LinkedList<(usize, i32)>) {
         for _ in 0..nempty { mem.push(None); }
         if nempty > 0 {
             let empty_pos = mem.len() as i32 - nempty as i32;
-            emptyblocks.push_back((empty_pos as usize, nempty as i32));
+            emptyblocks.push((empty_pos as usize, nempty as usize));
         }
     }
     (mem, emptyblocks)
@@ -37,45 +37,41 @@ fn compact1(memory: &mut Vec<Option<i32>>) {
     }
 }
 
-fn print_memory(memory: & Vec<Option<i32>>) {
-    for m in memory {
-        match m {
-            Some(x) => print!("{x}"),
-            None => print!(".")
-        }
+fn cur_block_range(memory: & Vec<Option<i32>>, pos: usize) -> (usize, usize) {
+    let mut start = pos as isize;
+    let mut end = pos;
+    while start >= 0 && memory[start as usize] == memory[pos] {
+        start -= 1;
     }
-    println!();
+    while end < memory.len() && memory[end] == memory[pos] {
+        end += 1;
+    }
+    ((start + 1) as usize, (end - 1) as usize)
 }
 
-fn compact2(memory: &mut Vec<Option<i32>>, emptyblocks: &mut LinkedList<(usize, i32)>) {
+fn compact2(memory: &mut Vec<Option<i32>>, emptyblocks: &mut Vec<(usize, usize)>) {
     let mut end = memory.len() - 1;
     while end > 0 {
-        if memory[end].is_some() {
-            let mut block_start = end;
-            let block_end = end;
-            while memory[block_end] == memory[block_start] {
-                block_start -= 1;
-                if block_start <= 0 { return; }
-            }
-            block_start += 1;
-
-            let block_size = (block_end - block_start) + 1;
-            for (pos, size) in emptyblocks.iter_mut() {
-                if *pos > block_start { break; }
-                if *size as usize >= block_size {
-                    for i in 0..block_size {
-                        memory[*pos + i] = memory[block_start + i];
-                        memory[block_start + i] = None;
-                    }
-                    *pos += block_size;
-                    *size -= block_size as i32;
-                    break;
-                }
-            }
-            end -= block_size;
-        } else {
+        if memory[end].is_none() {
             end -= 1;
+            continue;
         }
+
+        let (block_start, block_end) = cur_block_range(memory, end);
+        let block_size = block_end - block_start + 1;
+        for (pos, size) in emptyblocks.iter_mut() {
+            if *pos >= block_start { break; }
+            if *size >= block_size {
+                for i in 0..block_size {
+                    memory[*pos + i] = memory[block_start + i];
+                    memory[block_start + i] = None;
+                }
+                *pos += block_size;
+                *size -= block_size;
+                break;
+            }
+        }
+        end -= usize::min(block_size, end);
     }
 }
 
